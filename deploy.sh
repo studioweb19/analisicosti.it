e4fb1ce4d7d6408e0fe41a488a3d6e84de298bfc
studioweb19 <info@studioweb19.it>
2021-05-24 13:09:14 +0200

    sistemato deploy e messe chiavi ssh su server

---
+++ deploy.sh
@@ -0,0 +1,1 @@
+rsync -e ssh -avz --exclude 'index.html' * studioweb19enzoacuser@analisicosti.it:/var/www/clients/client1/web9/web
--- sincro.sh
+++
@@ -1,1 +0,0 @@
-rsync -e ssh -avz --exclude 'index.html' * enzoacuser@analisicosti.it:/var/www/clients/client6/web11/web
